7dca86893dbcc0a264daa7b34b29cbc8072b93d2
devreka28 <50713651+devreka28@users.noreply.github.com>
2024-08-11 18:59:52 +0100

    Create data_quality.py

---
+++ data_quality.py
@@ -0,0 +1,27 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                redshift_conn_id='',
+                table='',
+                *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id=redshift_conn_id
+        self.table=table
+
+    def execute(self, context):
+        self.log.info('Start Data Quality Check')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Data quality check failed. {table} returned no results")
+        else:
+            self.log.info('Data quality check passed. The table is not empty so loading was successful.')
+        self.log.info('Data quality check complete.')
