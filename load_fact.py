f398cc08cd47f48214c68b35e3a60c762bc69b17
devreka28 <50713651+devreka28@users.noreply.github.com>
2024-08-11 18:58:52 +0100

    Create load_fact.py

---
+++ load_fact.py
@@ -0,0 +1,38 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.secrets.metastore import MetastoreBackend
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+    drop_table= """
+        DROP TABLE IF EXISTS {}
+        """
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 insert_sql="",
+                 create_sql="",
+                 table="",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id=redshift_conn_id
+        self.create_sql=create_sql
+        self.insert_sql = insert_sql
+        self.table=table
+
+    def execute(self, context):
+        metastoreBackend = MetastoreBackend()
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        redshift_hook.run(f"DROP TABLE IF EXISTS {self.table}")
+        redshift_hook.run(self.create_sql)
+        
+        self.log.info('Fact table was created')
+
+        custom_sql= self.insert_sql
+        redshift_hook.run(custom_sql)
+
+        self.log.info(f"Data inserted into {self.table}")
+
+
+  
